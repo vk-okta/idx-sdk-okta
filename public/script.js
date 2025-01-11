e68b325a6455010f5f05bc91d46de115ac273c1a
vivekokta <vivek.giri@okta.com>
2025-01-11 13:25:56 +0530

    IDP List

--- public/script.js
+++ public/script.js
@@ -1,6 +1,10 @@
 let authClient = {};
 var appState = {};
-var config = {};
+var config = {
+  issuer: 'https://vivek-giri.oktapreview.com/oauth2/ausae177jfbCM7LBp1d7',
+  clientId: '0oaehju4utBnhFRvP1d7',
+  scopes: ['openid', 'profile', 'offline_access'],
+};
 
 async function transformAuthState(oktaAuth, authState) {
   if (!authState.isAuthenticated) {
@@ -19,7 +23,7 @@ async function transformAuthState(oktaAuth, authState) {
 
 // Wait for DOM content to be loaded before starting the app
 document.addEventListener('DOMContentLoaded', () => {
-  // this loads the config from the localstorage
+  // this loads the config from the sessionStorage
   loadConfig();
 
   // start the app
@@ -50,6 +54,8 @@ function main() {
   // this is needed if you want to refresh the page and stay on the
   // authenticated app
   authClient.start();
+
+  authClient.idx.start().then(handleTransaction).catch(showError);
 }
 
 function createAuthClient() {
@@ -67,7 +73,7 @@ function createAuthClient() {
       useInteractionCodeFlow: true,
     };
 
-    localStorage.setItem('config', JSON.stringify(showConfigData));
+    sessionStorage.setItem('config', JSON.stringify(showConfigData));
 
     document.getElementById('config-section').innerText = stringify(showConfigData);
   } catch (error) {
@@ -77,7 +83,7 @@ function createAuthClient() {
 }
 
 function loadConfig() {
-  const storedConfig = JSON.parse(localStorage.getItem('config'));
+  const storedConfig = JSON.parse(sessionStorage.getItem('config'));
 
   if (storedConfig) Object.assign(config, storedConfig);
 }
@@ -128,7 +134,7 @@ function submitConfig() {
   showSignInFormSection();
 
   Object.assign(config, { issuer, clientId, scopes });
-  createAuthClient()
+  createAuthClient();
 }
 
 function submitSignInUser() {
@@ -185,7 +191,12 @@ function handleTransaction(transaction) {
   switch (transaction.status) {
     case 'PENDING':
       if (transaction.nextStep.name === 'identify') {
-        renderDynamicSigninForm(transaction);
+        // renderDynamicSigninForm(transaction);
+
+        // check for available IDPs
+        const idpsList = transaction.availableSteps.filter((step) => step.name === 'redirect-idp');
+        showAvailableIdps(idpsList);
+
         break;
       }
 
@@ -308,10 +319,35 @@ function showMFA() {
       case 'select-authenticator-unlock-account':
         showUnlockAccountFormWithRemediators();
         break;
+      case 'redirect-idp':
+        console.log('work');
+        break;
       default:
         throw new Error(`TODO: showMfa handle nextStep: ${nextStep.name}`);
     }
   }
+}
+
+function showAvailableIdps(idpsList) {
+  const containerElement = document.getElementById('idp-button-section');
+  containerElement.style.display = 'block';
+
+  idpsList.forEach(function (elem) {
+    const idpLabel = elem.idp.name;
+    const idpLink = elem.href;
+
+    const el = document.createElement('a');
+    el.setAttribute('href', idpLink);
+    el.setAttribute('class', 'idp-button');
+
+    el.innerHTML = `Login with ${idpLabel}`;
+
+    containerElement.appendChild(el);
+
+    // Add a line break after the anchor tag
+    const br = document.createElement('br');
+    containerElement.appendChild(br);
+  });
 }
 
 function showAuthenticatorVerificationData() {
